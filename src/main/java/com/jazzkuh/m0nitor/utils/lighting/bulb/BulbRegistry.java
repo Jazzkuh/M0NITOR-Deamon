e05f6d18f7d5314fa6f8131fa89610193595eac9
jazz <jazzbauer77@gmail.com>
2025-08-22 18:48:42 +0200

    fixes2

--- src/main/java/com/jazzkuh/m0nitor/utils/lighting/bulb/BulbRegistry.java
+++ src/main/java/com/jazzkuh/m0nitor/utils/lighting/bulb/BulbRegistry.java
@@ -11,23 +11,7 @@ public class BulbRegistry {
 	private static final List<Bulb> bulbs = new ArrayList<>();
 
 	static {
-		bulbs.add(new Bulb("studio_led_strip2", "192.168.1.93"));
-
-		bulbs.add(new Bulb("living_desk", "192.168.1.81", "living", "scarlet"));
-
-		bulbs.add(new Bulb("living_hanging_one", "192.168.1.138", "living", "hanging", "scarlet"));
-		bulbs.add(new Bulb("living_hanging_two", "192.168.1.137", "living", "hanging", "scarlet"));
-		bulbs.add(new Bulb("living_hanging_three", "192.168.1.139", "living", "hanging", "scarlet"));
-
-		bulbs.add(new Bulb("living_yellow_lamp", "192.168.1.91", "living", "yellow_lamp", "scarlet"));
-		bulbs.add(new Bulb("living_standing", "192.168.1.88", "living", "standing", "scarlet"));
-
-		bulbs.add(new Bulb("kitchen_one", "192.168.1.89", "kitchen", "one", "warm_white"));
-		bulbs.add(new Bulb("kitchen_two", "192.168.1.90", "kitchen", "two", "warm_white"));
-		bulbs.add(new Bulb("kitchen_three", "192.168.1.80", "kitchen", "three", "warm_white"));
-
-		bulbs.add(new Bulb("cabinet_one", "192.168.1.77", "living", "cabinet", "warm_white"));
-		bulbs.add(new Bulb("cabinet_two", "192.168.1.203", "living", "cabinet", "warm_white"));
+		bulbs.add(new Bulb("studio_led_strip2", "192.168.2.47"));
 	}
 
 	public static List<Bulb> getAllBulbs() {
