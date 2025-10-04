42fabf6cab39daedccaf7374495ea4c081b51d67
jazz <jazzbauer77@gmail.com>
2025-10-04 17:22:39 +0200

    a

--- src/main/java/com/jazzkuh/m0nitor/modules/airlite/registry/ChannelTriggerRegistry.java
+++ src/main/java/com/jazzkuh/m0nitor/modules/airlite/registry/ChannelTriggerRegistry.java
@@ -15,15 +15,16 @@ import java.util.Map;
 
 public class ChannelTriggerRegistry {
 	private static Map<ChannelTrigger, Class<? extends TriggerAction>> triggers = new HashMap<>();
+    private static final int spotifyChannel = 7;
 
 	static {
 		registerAction(new ChannelTrigger(TriggerType.MICROPHONE_ON), OnAirLightTrigger.class);
 		registerAction(new ChannelTrigger(TriggerType.MICROPHONE_OFF), RegularLightTrigger.class);
-		registerAction(new ChannelTrigger(8, TriggerType.FADER_AND_CHANNEL_ON), MusicSkipStartTrigger.class);
-		registerAction(new ChannelTrigger(8, TriggerType.CHANNEL_OFF), MusicPauseTrigger.class);
-		registerAction(new ChannelTrigger(8, TriggerType.FADER_OFF), MusicPauseTrigger.class);
-		registerAction(new ChannelTrigger(8, TriggerType.FADER_OFF_CUE_ON), CueOnTrigger.class);
-		registerAction(new ChannelTrigger(8, TriggerType.FADER_OFF_CUE_OFF), CueOffTrigger.class);
+		registerAction(new ChannelTrigger(spotifyChannel, TriggerType.FADER_AND_CHANNEL_ON), MusicSkipStartTrigger.class);
+		registerAction(new ChannelTrigger(spotifyChannel, TriggerType.CHANNEL_OFF), MusicPauseTrigger.class);
+		registerAction(new ChannelTrigger(spotifyChannel, TriggerType.FADER_OFF), MusicPauseTrigger.class);
+		registerAction(new ChannelTrigger(spotifyChannel, TriggerType.FADER_OFF_CUE_ON), CueOnTrigger.class);
+		registerAction(new ChannelTrigger(spotifyChannel, TriggerType.FADER_OFF_CUE_OFF), CueOffTrigger.class);
 	}
 
 	public static void registerAction(ChannelTrigger channelTrigger, Class<? extends TriggerAction> triggerClass) {
